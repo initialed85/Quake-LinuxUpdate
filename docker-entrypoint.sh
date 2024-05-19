70df6d3dae9fc5c83a10e9bcaa02564d91a3a449
Edward Beech <initialed85@gmail.com>
2024-05-19 14:57:28 +0800

    Just infra tweaks

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,10 +2,11 @@
 
 set -e
 
-screen -a -A -S session -d -m bash -c './quake.x11 -dedicated 64 -port 26000 -noipx -debug -condebug -verbose -game frikbot +map dm6 +sv_aim 2 +saved1 1 +skill 0 ; pkill -9 -f gotty'
+screen -a -A -S session -d -m bash -c './quake.x11 -mem 256 -zone 2048 -dedicated 16 -port 26000 -noipx -game frikbot +map dm6 +sv_aim 1 +saved1 1 +skill 0 +sys_ticrate 0.05 ; pkill -9 -f gotty'
 
 ./gotty \
     --address 0.0.0.0 \
+    --port 7072 \
     --ws-origin '.*' \
     --permit-write \
     --reconnect \
