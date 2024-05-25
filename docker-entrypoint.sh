c653caec992b300c46ea44b1e86ca1c3212174b6
Edward Beech <initialed85@gmail.com>
2024-05-25 22:07:43 +0800

    Hmm not sure

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,13 +2,17 @@
 
 set -e
 
-screen -a -A -S session -d -m bash -c './quake.x11 -mem 256 -zone 2048 -dedicated 16 -port 26000 -noipx -game frikbot +map dm6 +sv_aim 1 +saved1 1 +skill 0 +sys_ticrate 0.05 ; pkill -9 -f gotty'
+quake_cmd='./quake.x11 -debug -condebug -mem 256 -zone 2048 -dedicated 16 -port 26000 -noipx -game frikbot +map dm6 +sv_aim 1 +saved1 1 +skill 0 +sys_ticrate 0.05'
 
-./gotty \
-    --address 0.0.0.0 \
-    --port 7072 \
-    --ws-origin '.*' \
-    --permit-write \
-    --reconnect \
-    --reconnect-time -1 \
-    screen -a -A -x session
+# screen -a -A -S session -d -m bash -c "${quake_cmd} ; pkill -9 -f gotty"
+
+# ./gotty \
+#     --address 0.0.0.0 \
+#     --port 7072 \
+#     --ws-origin '.*' \
+#     --permit-write \
+#     --reconnect \
+#     --reconnect-time -1 \
+#     screen -a -A -x session
+
+${quake_cmd}
