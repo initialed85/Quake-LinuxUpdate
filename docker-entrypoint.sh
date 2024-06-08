1a31ce4a328de22896f8e37639bd48798433c0b3
Edward Beech <initialed85@gmail.com>
2024-06-08 15:47:49 +0800

    WIP

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,15 +4,16 @@ set -e
 
 quake_cmd='./quake.x11 -debug -condebug -mem 256 -zone 2048 -dedicated 16 -port 26000 -noipx -game frikbot +map dm6 +sv_aim 1 +saved1 1 +skill 0 +sys_ticrate 0.05'
 
-# screen -a -A -S session -d -m bash -c "${quake_cmd} ; pkill -9 -f gotty"
+# uncomment this when hacking
+# ${quake_cmd}
 
-# ./gotty \
-#     --address 0.0.0.0 \
-#     --port 7072 \
-#     --ws-origin '.*' \
-#     --permit-write \
-#     --reconnect \
-#     --reconnect-time -1 \
-#     screen -a -A -x session
-
-${quake_cmd}
+# comment these two when hacking
+screen -a -A -S session -d -m bash -c "${quake_cmd} ; pkill -9 -f gotty"
+./gotty \
+    --address 0.0.0.0 \
+    --port 7072 \
+    --ws-origin '.*' \
+    --permit-write \
+    --reconnect \
+    --reconnect-time -1 \
+    screen -a -A -x session
